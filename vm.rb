9af5d9ed5c8f4a942b35742b0684678522219738
shouhei-yamamoto <elefish19920509@gmail.com>
2022-03-22 11:19:47 +0900

    READMEの作成

---
+++ vm.rb
@@ -0,0 +1,59 @@
+class Drink
+  attr_accessor :list
+  def initialize
+    @list = {
+      Coke: { price: 120, stock: 5},
+      Redbull: { price: 200, stock: 5},
+      Water: { price: 100, stock: 5}
+    }
+  end
+end
+
+class VendingMachine
+  attr_accessor :drink
+  MONEY = [10, 50, 100, 500, 1000].freeze
+
+  def initialize
+    @sale_amount = 0 
+    @slot_money = 0 
+    @drink = Drink.new
+  end
+
+  def current_slot_money
+    @slot_money
+  end
+
+  def slot_money(money)
+    return puts money unless MONEY.include?(money)
+    @slot_money += money
+  end
+
+  def return_money
+    puts @slot_money
+    @slot_money = 0
+  end
+
+  def sale_amount
+    @sale_amount
+  end
+
+  def purchase(drink_menu)
+    return "お金が足りません" unless @slot_money > drink.list[drink_menu][:price]
+    return "在庫がありません" unless drink.list[drink_menu][:stock] > 0
+    if @slot_money >= drink.list[drink_menu][:price] && drink.list[drink_menu][:stock] > 0
+       puts drink.list[drink_menu][:stock] -= 1
+       puts @slot_money -= drink.list[drink_menu][:price]
+       puts @sale_amount += drink.list[drink_menu][:price]
+    end
+  end
+
+  def purchasable_list
+    drink.list.map do |key, val|
+      key if val[:stock] > 0 && val[:price] <= current_slot_money
+    end.compact
+  end
+
+  def store(drink_menu)
+    puts drink.list[drink_menu][:stock] += 1
+  end
+end
